d7b199630c7d4d137e971f8acf7a182e8bffe537
Jack Williams <jack@inngest.com>
2023-05-31 13:22:04 +0000

    Fix typo when serving

--- server/api/inngest.ts
+++ server/api/inngest.ts
@@ -1,4 +1,4 @@
 import { serve } from "inngest/nuxt";
 import { inngest, functions } from "~~/inngest";
 
-export default defineEventHandler(serve(inngest, fns));
+export default defineEventHandler(serve(inngest, functions));
